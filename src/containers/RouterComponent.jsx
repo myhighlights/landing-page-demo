3e6faa6594d99f0404fe0c30b6085b02817fda06
Fran Aragón <franaragon@gransliving.com>
2024-03-26 16:33:14 +0100

    Remove comments

--- src/containers/RouterComponent.jsx
+++ src/containers/RouterComponent.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes,  Navigate } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Home from '../pages/Home';
-import Demo from '../pages/Demo';
-import Highlight from '../pages/Highlight';
-import Game from '../pages/Game';
-import HeaderDEMO from '../components/demo/header/Header';
 
 const RouterComponent = () => {
   return (
@@ -17,33 +13,6 @@ const RouterComponent = () => {
             <>
               <Header />
               <Home />
-            </>
-          }
-        />
-        <Route
-          path="/demo/games"
-          element={
-            <>
-              <HeaderDEMO />
-              <Demo />
-            </>
-          }
-        />
-        <Route
-          path="/demo/games/:id"
-          element={
-            <>
-              <HeaderDEMO />
-              <Game />
-            </>
-          }
-        />
-        <Route
-          path="/demo/games/:id/game/:videoParam"
-          element={
-            <>
-              <HeaderDEMO />
-              <Highlight />
             </>
           }
         />

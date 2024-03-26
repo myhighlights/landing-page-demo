debe34467d8797abfa23eca0bde66a26c95c596a
Fran Aragón <franaragon@gransliving.com>
2024-03-26 17:50:27 +0100

    Add structure

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,21 +2,44 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledGamePageContainer = styled.div`
-    height: 90vh;
-    display: flex; 
-    justify-content: center;
-    align-items: center;
+    height: 85vh;
+    display: flex;
+    flex-direction: column; 
+    justify-content: space-between;
     border: 1px solid red;
 `;
 
-const SelectedGameContainer = styled.div ``;
+const SelectedGameContainer = styled.div `
+    border: 1px solid green;
+    height: 30%;
+`;
+
+const SelectedGameOptionContainer = styled.div `
+    border: 1px solid yellow;
+    height: 60%;
+`;
+
+const SelectedGameButtonContainer = styled.div `
+    border: 1px solid blue;
+    height: 10%;
+`;
+
+const StyledButton = styled.div`
+    border: 1px solid red;
+`;
 
 const Game = () => {
     return (
         <StyledGamePageContainer>
             <SelectedGameContainer>
-
+            
             </SelectedGameContainer>
+            <SelectedGameOptionContainer>
+            
+            </SelectedGameOptionContainer>
+            <SelectedGameButtonContainer>
+                
+            </SelectedGameButtonContainer>
         </StyledGamePageContainer>
     );
 };

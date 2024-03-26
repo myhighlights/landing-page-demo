c36f7377053735ec80723ca68306063a8a119b47
Abel Osorio <osorioabel@gmail.com>
2024-03-26 20:41:53 +0100

    Remove blur from landing page

--- src/components/showCase/ShowCase.jsx
+++ src/components/showCase/ShowCase.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import blur from '../../assets/showCase/blur.png';
 import VideoContainer from './VideoContainer';
 
 const ShowCaseContainer = styled.div`
     height: 33vh;
     position: relative;
+    padding-bottom: 0.8rem;
 `;
 
 const StyledBlurContainer = styled.div`
@@ -20,16 +20,10 @@ const StyledBlurContainer = styled.div`
     padding-top: .8rem;
 `;
 
-const StyledBlur = styled.img`
-    position: absolute;
-    width: 120%;
-`;
-
 const ShowCase = () => {
     return (
         <ShowCaseContainer id='showCase' className='showCase-desktop'>
            <StyledBlurContainer className='blur-desktop'>
-                <StyledBlur src={blur} />
            </StyledBlurContainer>
            <VideoContainer />
         </ShowCaseContainer>

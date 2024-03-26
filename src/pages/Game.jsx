dca8fc5f6bd841cd4426afc1834acdcfa97cc65d
Fran Aragón <franaragon@gransliving.com>
2024-03-26 18:49:14 +0100

    Add first section

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,17 +4,19 @@ import LeftVector from "../assets/demo/game/leftVector.png";
 import LeftVectorBlack from "../assets/demo/game/leftVectorBlack.png";
 import RightVector from "../assets/demo/game/rightVector.png";
 import RightVectorBlack from "../assets/demo/game/rightVectorBlack.png";
+import fkmbLogo from "../assets/demo/game/fkmb.png";
+import dynamoLogo from "../assets/demo/game/dynamo.png";
 
 const StyledGamePageContainer = styled.div`
   height: 85vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  border: 1px solid red;
 `;
 
 const SelectedGameContainer = styled.div`
-  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
   height: 30%;
 `;
 
@@ -23,15 +25,71 @@ const SelectedGameTitle = styled.h3`
   font-size: 25px;
   font-weight: 500;
   line-height: 40.8px;
+  margin-bottom: 1rem;
+`;
+
+const SelectedGameDataContainer = styled.div`
+  display: flex;
+  height: 10rem;
+  align-items: center;
+  justify-content: center;
+  color: rgba(255, 255, 255, 0.6);
+
+  > div {
+    margin-left: 4rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 15.5rem;
+    height: 100%;
+    padding: 0;
+  }
+`;
+
+const StyledHomeContainer = styled.div``;
+
+const StyledResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-family: Onest;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 20.4px;
+  text-align: left;
+  color: #9c9c9c;
+`;
+
+const StyledResult = styled.div`
+  font-family: NBArchitekt;
+  font-size: 60px;
+  font-weight: 400;
+  line-height: 80px;
+  text-align: center;
+  color: white;
+`;
+
+const StyledAwayContainer = styled.div``;
+
+const StyledTeamName = styled.span`
+  font-family: Onest;
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 30.6px;
+  letter-spacing: 0.5px;
+  white-space: ${({ whiteSpace }) => (!whiteSpace ? "nowrap" : "")};
+  margin: 0 1rem;
+`;
+
+const StyledTeamLogo = styled.img`
+  width: 5rem;
 `;
 
 const SelectedGameOptionContainer = styled.div`
-  border: 1px solid yellow;
   height: 60%;
 `;
 
 const SelectedGameButtonContainer = styled.div`
-  border: 1px solid blue;
   height: 10%;
   display: flex;
   justify-content: end;
@@ -77,6 +135,25 @@ const Game = () => {
     <StyledGamePageContainer>
       <SelectedGameContainer>
         <SelectedGameTitle>Selected Game</SelectedGameTitle>
+        <SelectedGameDataContainer>
+          <StyledHomeContainer>
+            <StyledTeamName whiteSpace={false}>
+              FK Mladá Boleslav
+            </StyledTeamName>
+            <StyledTeamLogo src={fkmbLogo} />
+          </StyledHomeContainer>
+          <StyledResultContainer>
+            <div>Saturday - March, 3</div>
+            <StyledResult>3:1</StyledResult>
+            <div>Fortuna:Liga</div>
+          </StyledResultContainer>
+          <StyledAwayContainer>
+            <StyledTeamLogo src={dynamoLogo} />
+            <StyledTeamName whiteSpace={true}>
+              SK Dynamo České Budějovice
+            </StyledTeamName>
+          </StyledAwayContainer>
+        </SelectedGameDataContainer>
       </SelectedGameContainer>
       <SelectedGameOptionContainer></SelectedGameOptionContainer>
       <SelectedGameButtonContainer>
